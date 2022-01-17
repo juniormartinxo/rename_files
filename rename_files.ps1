f27a272397badbb078f442817bee320aa8804e88
Junior Martins <amjr.box@gmail.com>
2022-01-17 07:37:51 -0300

    Atualização das perguntas que permitirão o filtro pela extensão

--- rename_files.ps1
+++ rename_files.ps1
@@ -1,8 +1,18 @@
 $path = Get-Location
-$path_app = Read-Host "Caminho do aplicativo"
+$path_app = "D:\webapps\rename_files"
+#Read-Host "Caminho do aplicativo"
 $sufixo = Read-Host "Sufixo dos arquivos"
 $replace = Read-Host "Palavras para substituir (REGEX)"
-$enumerate = Read-Host "Enumerar? [s|n]"
+$extension = Read-Host "Extensao dos arquivos (deixe em branco para todos)"
+if ($extension -eq '') {
+	$extension = "all"
+}
+$enumerate = Read-Host "Enumerar [s|n]"
+if ($enumerate -ieq 's') {
+	$numstart = Read-Host "Enumerar a partir de"
+}else{
+	$numstart = 1
+}
 Set-Location $path_app
-node index --path=$path --suffix=$sufixo --replace=$replace --enumerate=$enumerate
+node index --path=$path --suffix=$sufixo --replace=$replace --enumerate=$enumerate --numstart=$numstart --extension=$extension
 Set-Location $path
